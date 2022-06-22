5d196185cf076bc567baaa8becd8829959f2db3d
Grzegorz Malinowski <gmalinowski@protonmail.com>
2022-06-23 00:33:48 +0200

    init

---
+++ lib/avatarix.ex
@@ -0,0 +1,18 @@
+defmodule Avatarix do
+  @moduledoc """
+  Documentation for `Avatarix`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Avatarix.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/avatarix_test.exs
@@ -0,0 +1,8 @@
+defmodule AvatarixTest do
+  use ExUnit.Case
+  doctest Avatarix
+
+  test "greets the world" do
+    assert Avatarix.hello() == :world
+  end
+end
